a6619c130d7a2c5cae5c76bcb6e1dbedff0a93fb
lucasbatista2408 <lucasbatista2408@gmail.com>
2022-04-18 16:47:26 -0300

    final-version

--- javascript/engine.js
+++ javascript/engine.js
@@ -1,33 +1,39 @@
 let listMessages = []
-let nickname = prompt("Qual seu nickname?")
+let nickname;
+
 // LOGIN STAGE
 
-const onlineRequest = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',{
+function login(){
+  nickname = document.querySelector(".loginInfo").value
+  console.log(nickname)
+  const onlineRequest = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',{
     name: `${nickname}`
-  } )
+  })
+  
+  let hide = document.querySelector(".login-page")
+  hide.classList.add('hidden')
 
   onlineRequest.then(keepOnline);
+  console.log("logou")
   onlineRequest.catch(onlineRequestError);
 
   function onlineRequestError(request){
       console.log("logged in")
   }
+}
 
 // KEEP ONLINE STAGE
 function keepOnline(){
-    
      const statusOnline = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {
          name: `${nickname}`
      })
 
-     statusOnline.then(console.log('suceeded'));
+     statusOnline.then(requestMensagem);
      //requestMensagem()
 }
 setInterval(keepOnline, 5000)
 
 // GET MESSAGES
-
-
 
 function requestMensagem(){
   const request = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
@@ -39,24 +45,29 @@ function requestMensagem(){
     console.log('chamou a function')
   }
 }
-requestMensagem()
-setInterval(requestMensagem, 7000)
+
 
 function addReceivedMessage(){
   let texto = document.querySelector(".chat")
   for (i = 0; i < listMessages.length; i++){
     if(listMessages[i].type == 'status'){
-      document.querySelector(".chat").innerHTML += `<div class='message-status'>
+      texto.innerHTML += `<div class='message-status'>
       <span><span style="color: lightgray">(${listMessages[i].time})</span> <span style="font-weight: 700;">${listMessages[i].from}</span> ${listMessages[i].text}</span>
     </div>` 
     }
     if(listMessages[i].type == 'message'){
-      document.querySelector(".chat").innerHTML += `<div class='message'>
+      texto.innerHTML += `<div class='message'>
         <span><span style="color: lightgray">(${listMessages[i].time})</span> <span style="font-weight: 700;">${listMessages[i].from}</span> para <span style="font-weight: 700;">${listMessages[i].to}</span>: ${listMessages[i].text}</span>
       </div>`
     }
+    if(listMessages[i].type == 'private_message'){
+      texto.innerHTML += `<div class='private-message'>
+      <span><span style="color: lightgray">(${listMessages[i].time})</span> <span style="font-weight: 700;">${listMessages[i].from}</span> para <span style="font-weight: 700;">${listMessages[i].to}</span>: ${listMessages[i].text}</span>
+    </div>`
+    }
   }
-  document.querySelector(".chat").scrollIntoView(false)
+  const lastMessage = document.querySelector('.chat div:last-child');
+  lastMessage.scrollIntoView();
 }
 
 function limparConversa(){
@@ -64,7 +75,7 @@ function limparConversa(){
   chat.innerHTML = ""
   addReceivedMessage
 }
-setInterval(limparConversa, 7000)
+setInterval(limparConversa, 5000)
 
 function sendMessage(){
   let send = document.querySelector(".sendMsg");
